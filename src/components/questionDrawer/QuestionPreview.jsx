c63c9232ab3ba3dc40fae9ba09bd92b59f28a559
Vishnu Rana <mvishnurana@gmail.com>
2020-12-30 21:38:09 +0800

    Added logic for fixing the expand option

--- src/components/questionDrawer/QuestionPreview.jsx
+++ src/components/questionDrawer/QuestionPreview.jsx
@@ -7,35 +7,31 @@ class QuestionPreview extends Component {
     constructor(props) {
         super(props); 
         this.state = {
-            expanded: false,
+            expanded: true,
             expandedID: null,
         }
-    }
-
-    toggleExpand(id) {
-        this.setState({ expanded: true }, () => console.log(id))
     }
 
     render() {
         const { questionAnswerPair } = this.props; 
         const { expanded, expandedID } = this.state; 
+
         return questionAnswerPair.map(({ id, question, options }, index) => 
-        <div key={id} style={{ marginLeft: '10%'}}>
+        <div key={id} style={{ margin: '0 10%'}}>
             <div style={{ display: 'flex'}}>
                 <h3 style={{marginRight: '1rem', minWidth: '3rem'}}>Q {index + 1}.</h3>
                 <h3 className="questionOptions" style={{ marginBottom: 0 }}>{question}</h3>
             </div>
             <IconButton
                 aria-label="show more"
+                onClick={() => this.setState({ expandedID: id, expanded: !expanded })} 
                 style={expandedID === id ? { backgroundColor: 'black', color: 'white' }: null }
             >
-            <ExpandMoreIcon 
-                onClick={() => this.setState({ expanded: !expanded, expandedID: id })} 
-                style={(id === expandedID) ? {transform: 'rotate(0deg)' }: {transform: 'rotate(180deg)' }} 
-            />
+                <ExpandMoreIcon 
+                    style={(id === expandedID) ? {transform: 'rotate(0deg)' }: {transform: 'rotate(180deg)' }} 
+                />
             </IconButton>
-            {console.log(expandedID, id)}
-            <Collapse in={expandedID === id} timeout="auto" unmountOnExit>
+            <Collapse in={expandedID === id}>
                 {options.map((option) => <div>
                         {option}
                     </div>
